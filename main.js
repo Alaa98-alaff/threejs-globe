cd9c6f4c5487f8b3a7033b93448b770e82c97ee1
Alaa Alaff <travel98_alaff@hotmail.com>
2022-05-02 09:02:23 +0300

    increase the sphere resulation

--- main.js
+++ main.js
@@ -7,16 +7,20 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true }); // antialias => make the border fade with bc
 
 renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio); // make the shapes smooth & hight resultion
 document.body.append(renderer.domElement);
 
 const sphereGeomatry = new THREE.SphereGeometry(5, 50, 50);
-const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const sphereMaterial = new THREE.MeshBasicMaterial({
+  // color: 0xff0000
+  map: new THREE.TextureLoader().load("./img/globe.jpg"), // to put the earth uv on the sphere
+});
 const sphereMesh = new THREE.Mesh(sphereGeomatry, sphereMaterial);
 scene.add(sphereMesh);
-camera.position.z = 10;
+camera.position.z = 15;
 
 function animate() {
   requestAnimationFrame(animate);

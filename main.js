6149c2ff7e1bd54da083e109bdd1725406063928
Alaa Alaff <travel98_alaff@hotmail.com>
2022-05-02 08:48:36 +0300

    set new sphere mesh

--- main.js
+++ main.js
@@ -12,6 +12,12 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(innerWidth, innerHeight);
 document.body.append(renderer.domElement);
 
+const sphereGeomatry = new THREE.SphereGeometry(5, 50, 50);
+const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const sphereMesh = new THREE.Mesh(sphereGeomatry, sphereMaterial);
+scene.add(sphereMesh);
+camera.position.z = 10;
+
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);

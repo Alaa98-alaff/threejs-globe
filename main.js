f4878daee2b568c18330c76d6a65b1efa197905a
Alaa Alaff <travel98_alaff@hotmail.com>
2022-05-02 09:48:31 +0300

    create vertex and fragment shaders

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import vertexShader from "./shaders/vertex.glsl"; // vite vite-plugin-string installed to supprot import
+import fragmentShader from "./shaders/fragment.glsl";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -14,9 +16,9 @@ renderer.setPixelRatio(devicePixelRatio); // make the shapes smooth & hight resu
 document.body.append(renderer.domElement);
 
 const sphereGeomatry = new THREE.SphereGeometry(5, 50, 50);
-const sphereMaterial = new THREE.MeshBasicMaterial({
-  // color: 0xff0000
-  map: new THREE.TextureLoader().load("./img/globe.jpg"), // to put the earth uv on the sphere
+const sphereMaterial = new THREE.ShaderMaterial({
+  vertexShader,
+  fragmentShader, // filling in the space between, loop over every pixel between the vertecies and set it color rqual to value
 });
 const sphereMesh = new THREE.Mesh(sphereGeomatry, sphereMaterial);
 scene.add(sphereMesh);

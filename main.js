3aa9ecdaaf3f615e2586fca549a8a5b139a1fe6a
Alaa Alaff <travel98_alaff@hotmail.com>
2022-05-03 00:05:58 +0300

    add atmosphere with second shader

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import vertexShader from "./shaders/vertex.glsl"; // vite vite-plugin-string installed to supprot import
 import fragmentShader from "./shaders/fragment.glsl";
+import atmosphereVertexShader from "./shaders/atmosphereVertex.glsl";
+import atmosphereFragmentShader from "./shaders/atmosphereFragment.glsl";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -15,11 +17,13 @@ renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(devicePixelRatio); // make the shapes smooth & hight resultion
 document.body.append(renderer.domElement);
 
+// create a sphere
 const sphereGeomatry = new THREE.SphereGeometry(5, 50, 50);
 const sphereMaterial = new THREE.ShaderMaterial({
   vertexShader,
   fragmentShader, // filling in the space between, loop over every pixel between the vertecies and set it color rqual to value
   uniforms: {
+    // texture send to the fragment and used there => think as porps in react
     globeTexture: {
       value: new THREE.TextureLoader().load("./img/globe.jpg"),
     },
@@ -27,11 +31,25 @@ const sphereMaterial = new THREE.ShaderMaterial({
 });
 const sphereMesh = new THREE.Mesh(sphereGeomatry, sphereMaterial);
 scene.add(sphereMesh);
+
+// create a atmosphere
+const atmosphereGeomatry = new THREE.SphereGeometry(5, 50, 50);
+const atmosphereMaterial = new THREE.ShaderMaterial({
+  vertexShader: atmosphereVertexShader,
+  fragmentShader: atmosphereFragmentShader, // filling in the space between, loop over every pixel between the vertecies and set it color rqual to value
+  blending: THREE.AdditiveBlending, // to blend/ mix it with the sphere
+  side: THREE.BackSide, // to show the shadows in the backside
+});
+const atmosphereMesh = new THREE.Mesh(atmosphereGeomatry, atmosphereMaterial);
+atmosphereMesh.scale.set(1.1, 1.1, 1.1);
+scene.add(atmosphereMesh);
+
 camera.position.z = 15;
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
+  sphereMesh.rotation.y += 0.001;
 }
 
 animate();
--- shaders/atmosphereVertex.glsl
+++ shaders/atmosphereVertex.glsl
@@ -1,9 +1,7 @@
-varying vec2 vertexUV;
 varying vec3 veretexNormal;
 
 void main () {
- vertexUV= uv;
- // to make the colors equal in all vertexes
+     // to make the colors equal in all vertexes
  veretexNormal= normalize(normalMatrix * normal);
 
  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );

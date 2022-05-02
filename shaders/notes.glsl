65d32a38cd8964f760662a7361d702bb60ac898a
Alaa Alaff <travel98_alaff@hotmail.com>
2022-05-02 23:24:58 +0300

    hanlde add shade of blue by using vertexNromal

---
+++ shaders/notes.glsl
@@ -0,0 +1,7 @@
+// https://threejs.org/docs/#api/en/renderers/webgl/WebGLProgram
+// dot book of shaders => shaders docs
+
+// Mat4 :https://thebookofshaders.com/glossary/?search=mat4
+// Texture2D : https://thebookofshaders.com/glossary/?search=texture2D
+// Dot Operator: https://thebookofshaders.com/glossary/?search=dot
+// Normalize: https://thebookofshaders.com/glossary/?search=normalize
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,7 +1,9 @@
-// https://threejs.org/docs/#api/en/renderers/webgl/WebGLProgram
 varying vec2 vertexUV;
+varying vec3 veretexNormal;
 
 void main () {
  vertexUV= uv;
+ veretexNormal= normal;
+
  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 }
